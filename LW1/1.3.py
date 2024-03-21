57e9b8c0f79864f72c6942b18417fc4565af0ca2
M1cleer <sponge.905@yandex.ru>
2024-03-21 12:29:58 +0300

    1.3 ex + another way of solving

--- LW1/1.3.py
+++ LW1/1.3.py
@@ -12,6 +12,11 @@ def is_palindrome_lat(str):
 
     return False
 
+
+# def another_way(str):
+#     lat_chars = "".join(char for char in str if char >= "a" and char <= "s")
+#     return lat_chars == lat_chars[::-1]
+
 str = "abcфафасяABCDdcba"
 
 print(is_palindrome_lat(str))
