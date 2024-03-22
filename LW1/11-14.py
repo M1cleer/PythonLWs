8f5a6cd754185225221a108dd9acb68990c127fd
M1cleer <sponge.905@yandex.ru>
2024-03-22 22:10:15 +0300

    11-14.2

--- LW1/11-14.py
+++ LW1/11-14.py
@@ -1,3 +1,5 @@
+
+# Задача 1
 def is_vowel(ch) :
 	ch = ch.upper()
 	return (ch == 'А' or ch == 'У'or ch == 'О' or ch == 'Ы'or ch == 'И'
@@ -16,7 +18,23 @@ def count_diff(string) :
 def sort_by_diff(inp_list):
 	return sorted(inp_list, key=lambda s: count_diff(s))
 
-inp_list = [ "аао", "стол","полёт", "семнадцать" ]
+# Задача 2
+def ascii_weight(string):
+    total_weight = sum(ord(i) for i in string)
+    return total_weight / len(string)
 
-print(sort_by_diff(inp_list))
+def sqr_dev(first_wt, wt):
+    return (wt - first_wt) ** 2
+
+def sort_strings_by_criteria(inp_list):
+    first_wt = ascii_weight(inp_list[0])
+    
+    sorted_strings = sorted(inp_list, key=lambda x: sqr_dev(first_wt, ascii_weight(x)))
+    
+    return sorted_strings
+
+inp_list = ["weight", "ascii", "sorting", "ASCII"]
+sorted_list = sort_strings_by_criteria(inp_list)
+print(sorted_list)
+
 
